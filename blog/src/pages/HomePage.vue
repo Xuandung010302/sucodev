0aae9141dc12be8fbbb697677f6cf98fb3eb9327
Xuan <daoxuandung010302@gmail.com>
2024-12-22 01:27:46 +0700

    feat/APIBE+CallAPI

--- blog/src/pages/HomePage.vue
+++ blog/src/pages/HomePage.vue
@@ -2,213 +2,69 @@
     <div class="bg-custom-bg h-100vh mx-auto">
         <div class="max-w-custom mx-auto pt-[37px]">
             <div class="max-w-custom bg-white mx-auto xl:h-[83px] flex items-center justify-start">
-            <span class="ml-11 text-txtGr text-lg leading-[50px]">Type something…</span>
-            <img src="@/components/icons/Icon.svg" alt="Search" class="h-6 w-auto ml-auto mr-6" />
+                <span class="ml-11 text-txtGr text-lg leading-[50px]">Type something…</span>
+                <img src="@/components/icons/Icon.svg" alt="Search" class="h-6 w-auto ml-auto mr-6" />
             </div>
             <div class="mt-10 mb-[16.8px] text-txtGr text-xs">
                 ALL POST
             </div>
-            <div class="flex grid-cols-2">
-                <div class="">
-                    <img src="@/components/images/image1.svg" alt="image1" class="mr-6 transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer" />
+            <div v-if="posts.length" class="grid grid-cols-2 gap-10">
+                <div v-for="(post, index) in posts" :key="index">
+                    <img :src="`http://localhost:3000/backend/images/${post.image}`" alt="Post Image" class="mr-6 transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer">
                     <div class="flex mt-5">
-                        <span class="text-txtGr">BY</span>&nbsp;&nbsp;TOMAS LAURINAVICIUS&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;DESIGN PROCESS
+                        <span class="text-txtGr">BY</span>&nbsp;&nbsp;{{ post.author }}&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;{{ post.category }}
                     </div>
                     <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px]">
-                        How to Reinstall WordPress: 5 Different Methods Depending On Your Needs
+                        {{ post.title }}
                     </div>
                     <div class="text-txtGr text-base mt-[-13px]">
-                        User research is the reality check every project needs. Here’s our guide <br> to why you should be doing it — and how to get started.
-                    </div>
-                </div>
-                <div class="">
-                    <img src="@/components/images/image2.svg" alt="image2" class="transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer" />
-                    <div class="flex mt-5">
-                        <span class="text-txtGr">BY</span>&nbsp;&nbsp;TOMAS LAURINAVICIUS&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;DESIGN PROCESS
-                    </div>
-                    <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px]">
-                        Web page layout 101: website anatomy every designer needs to learn
+                        {{ post.description }}
                     </div>
                     <div class="text-txtGr text-base mt-[-13px]">
-                        User research is the reality check every project needs. Here’s our guide <br> to why you should be doing it — and how to get started.
+                        {{ post.excerpt }}
                     </div>
                 </div>
             </div>
-            <!-- 2 -->
-            <div class="flex grid-cols-2 mt-[50px]">
-                <div class="">
-                    <img src="@/components/images/image3.svg" alt="image3" class="mr-6 transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer" />
-                    <div class="flex mt-5">
-                        <span class="text-txtGr">BY</span>&nbsp;&nbsp;TOMAS LAURINAVICIUS&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;DESIGN PROCESS
-                    </div>
-                    <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px]">
-                        Website Downtime: Applicable Tips on How to Prevent It                    
-                    </div>
-                    <div class="text-txtGr text-base mt-[-13px]">
-                        User research is the reality check every project needs. Here’s our guide <br> to why you should be doing it — and how to get started.
-                    </div>
-                </div>
-                <div class="">
-                    <img src="@/components/images/image4.svg" alt="image4" class="transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer" />
-                    <div class="flex mt-5">
-                        <span class="text-txtGr">BY</span>&nbsp;&nbsp;TOMAS LAURINAVICIUS&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;DESIGN PROCESS
-                    </div>
-                    <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px]">
-                        6 Ways to Leverage Facebook for Marketing Success
-                    </div>
-                    <div class="text-txtGr text-base mt-[-13px]">
-                        User research is the reality check every project needs. Here’s our guide <br> to why you should be doing it — and how to get started.
-                    </div>
-                </div>
+            <div v-else class="text-center mt-10">
+                <p>Loading posts...</p>
             </div>
-            <!-- 3 -->
-            <div class="flex grid-cols-2 mt-[50px]">
-                <div class="">
-                    <img src="@/components/images/image5.svg" alt="image5" class="mr-6 transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer" />
-                    <div class="flex mt-5">
-                        <span class="text-txtGr">BY</span>&nbsp;&nbsp;TOMAS LAURINAVICIUS&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;DESIGN PROCESS
-                    </div>
-                    <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px]">
-                        Updating WooCommerce: A Safe and In-Depth Guide
-                    </div>
-                    <div class="text-txtGr text-base mt-[-13px]">
-                        User research is the reality check every project needs. Here’s our guide <br> to why you should be doing it — and how to get started.
-                    </div>
-                </div>
-                <div class="">
-                    <img src="@/components/images/image6.svg" alt="image6" class="transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer" />
-                    <div class="flex mt-5">
-                        <span class="text-txtGr">BY</span>&nbsp;&nbsp;TOMAS LAURINAVICIUS&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;DESIGN PROCESS
-                    </div>
-                    <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px]">
-                        WP-CLI v2 – Managing WordPress From the Terminal
-                    </div>
-                    <div class="text-txtGr text-base mt-[-13px]">
-                        User research is the reality check every project needs. Here’s our guide <br> to why you should be doing it — and how to get started.
-                    </div>
-                </div>
-            </div>
-            <!-- 4 -->
-            <div class="flex grid-cols-2 mt-[50px]">
-                <div class="">
-                    <img src="@/components/images/image7.svg" alt="image7" class="mr-6 transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer" />
-                    <div class="flex mt-5">
-                        <span class="text-txtGr">BY</span>&nbsp;&nbsp;TOMAS LAURINAVICIUS&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;DESIGN PROCESS
-                    </div>
-                    <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px]">
-                        Social Media Image Sizes and Quick Tips for WordPress Users
-                    </div>
-                    <div class="text-txtGr text-base mt-[-13px]">
-                        User research is the reality check every project needs. Here’s our guide <br> to why you should be doing it — and how to get started.
-                    </div>
-                </div>
-                <div class="">
-                    <img src="@/components/images/image8.svg" alt="image8" class="transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer" />
-                    <div class="flex mt-5">
-                        <span class="text-txtGr">BY</span>&nbsp;&nbsp;TOMAS LAURINAVICIUS&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;DESIGN PROCESS
-                    </div>
-                    <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px]">
-                        How to Create Outstanding Long-Form Articles in WordPress
-                    </div>
-                    <div class="text-txtGr text-base mt-[-13px]">
-                        User research is the reality check every project needs. Here’s our guide <br> to why you should be doing it — and how to get started.
-                    </div>
-                </div>
-            </div>
-            <!-- 5 -->
-            <div class="flex grid-cols-2 mt-[50px]">
-                <div class="">
-                    <img src="@/components/images/image9.svg" alt="image9" class="mr-6 transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer" />
-                    <div class="flex mt-5">
-                        <span class="text-txtGr">BY</span>&nbsp;&nbsp;TOMAS LAURINAVICIUS&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;DESIGN PROCESS
-                    </div>
-                    <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px]">
-                        How to Use Fiverr to Reduce Business Busywork
-                    </div>
-                    <div class="text-txtGr text-base mt-[-13px]">
-                        User research is the reality check every project needs. Here’s our guide <br> to why you should be doing it — and how to get started.
-                    </div>
-                </div>
-                <div class="">
-                    <img src="@/components/images/image10.svg" alt="image10" class="transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer" />
-                    <div class="flex mt-5">
-                        <span class="text-txtGr">BY</span>&nbsp;&nbsp;TOMAS LAURINAVICIUS&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;DESIGN PROCESS
-                    </div>
-                    <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px]">
-                        Email Marketing Best Practices for Sending Better Emails
-                    </div>
-                    <div class="text-txtGr text-base mt-[-13px]">
-                        User research is the reality check every project needs. Here’s our guide <br> to why you should be doing it — and how to get started.
-                    </div>
-                </div>
-            </div>
-            <!-- 6 -->
-            <div class="flex grid-cols-2 mt-[50px]">
-                <div class="">
-                    <img src="@/components/images/image11.svg" alt="image11" class="mr-6 transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer" />
-                    <div class="flex mt-5">
-                        <span class="text-txtGr">BY</span>&nbsp;&nbsp;TOMAS LAURINAVICIUS&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;DESIGN PROCESS
-                    </div>
-                    <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px]">
-                        Google Site Verification: 7 Ways to Verify With Search Console
-                    </div>
-                    <div class="text-txtGr text-base mt-[-13px]">
-                        User research is the reality check every project needs. Here’s our guide <br> to why you should be doing it — and how to get started.
-                    </div>
-                </div>
-                <div class="">
-                    <img src="@/components/images/image13.svg" alt="image13" class="transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer" />
-                    <div class="flex mt-5">
-                        <span class="text-txtGr">BY</span>&nbsp;&nbsp;TOMAS LAURINAVICIUS&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;DESIGN PROCESS
-                    </div>
-                    <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px]">
-                        Twitter Marketing Strategies to Beef up Your Social Game
-                    </div>
-                    <div class="text-txtGr text-base mt-[-13px]">
-                        User research is the reality check every project needs. Here’s our guide <br> to why you should be doing it — and how to get started.
-                    </div>
-                </div>
-            </div>
-            <!-- 7 -->
-            <div class="flex grid-cols-2 mt-[50px]">
-                <div class="">
-                    <img src="@/components/images/image14.svg" alt="image14" class="mr-6 transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer" />
-                    <div class="flex mt-5">
-                        <span class="text-txtGr">BY</span>&nbsp;&nbsp;TOMAS LAURINAVICIUS&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;DESIGN PROCESS
-                    </div>
-                    <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px]">
-                        The Ultimate Step-by-Step Guide on Website Usability Testing
-                    </div>
-                    <div class="text-txtGr text-base mt-[-13px]">
-                        User research is the reality check every project needs. Here’s our guide <br> to why you should be doing it — and how to get started.
-                    </div>
-                </div>
-                <div class="">
-                    <img src="@/components/images/image12.svg" alt="image12" class="transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer" />
-                    <div class="flex mt-5">
-                        <span class="text-txtGr">BY</span>&nbsp;&nbsp;TOMAS LAURINAVICIUS&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;DESIGN PROCESS
-                    </div>
-                    <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px]">
-                        8 Ways To Leverage LinkedIn for Marketing Success
-                    </div>
-                    <div class="text-txtGr text-base mt-[-13px]">
-                        User research is the reality check every project needs. Here’s our guide <br> to why you should be doing it — and how to get started.
-                    </div>
-                </div>
-            </div>
-
             <div class="mt-[182px] flex justify-center">
-                <button class="mb-[155px] w-[204px] h-[70px] rounded-[40px] text-[#FF5480] border-2 border-[#FF5480] font-semibold hover:bg-opacity-90 text-2xl hover:bg-[#FF5480] hover:text-white transform transition-transform duration-300 hover:scale-105">
+                <button @click="loadMorePosts" class="mb-[155px] w-[204px] h-[70px] rounded-[40px] text-[#FF5480] border-2 border-[#FF5480] font-semibold hover:bg-opacity-90 text-2xl hover:bg-[#FF5480] hover:text-white transform transition-transform duration-300 hover:scale-105">
                     Load More
                 </button>
             </div>
         </div>
     </div>
-  </template>
+</template>
   
-  <script>
-  export default {
-    name: "HomePage",
-  };
-  </script>
+<script>
+    import axios from "axios";
+  
+    export default {
+        name: "HomePage",
+        data() {
+        return {
+            posts: [],
+            currentPage: 1,
+        };
+        },
+        created() {
+        this.fetchPosts();
+        },
+        methods: {
+        async fetchPosts() {
+            try {
+            const response = await axios.get(`http://localhost:3000/posts?page=${this.currentPage}`);
+            this.posts = [...this.posts, ...response.data];
+            } catch (error) {
+            console.error("Error fetching posts:", error);
+            }
+        },
+        loadMorePosts() {
+            this.currentPage++;
+            this.fetchPosts();
+        },
+        },
+    };
+</script>
   
