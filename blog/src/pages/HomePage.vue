4a6bbd84029e5623265eeef4caf51682eb16677e
Xuan <daoxuandung010302@gmail.com>
2024-12-22 04:34:11 +0700

    feat/update-load-4-post

--- blog/src/pages/HomePage.vue
+++ blog/src/pages/HomePage.vue
@@ -1,44 +1,32 @@
 <template>
-    <div class="bg-custom-bg h-100vh mx-auto">
-        <div class="max-w-custom mx-auto pt-[37px]">
-            <div class="max-w-custom bg-white mx-auto h-[83px] flex items-center justify-start">
-                <input 
-                v-model="type" 
-                @input="searchPosts"
-                type="text" 
-                class="pl-11 w-full border-gray-300 rounded-lg h-[83px]"
-                placeholder="Type Something..."
-                />
-                <img src="@/components/icons/Icon.svg" alt="Search" class="h-6 w-auto ml-auto mr-6" />
+    <div class="bg-custom-bg h-auto mx-auto p-8">
+        <div class="max-w-custom mx-auto">
+            <div class="text-center">
+                <div class="mt-10 mb-[16.8px] text-txtGr text-xs text-center lg:text-start">
+                    ALL POST
+                </div>
             </div>
-            <div class="mt-10 mb-[16.8px] text-txtGr text-xs text-center lg:text-start">
-                ALL POST
-            </div>
-
-            <div v-if="posts.length" class="lg:flex-none grid grid-cols-1 gap-10 lg:grid-cols-2 lg:gap-10 justify-center mx-auto lg:mx-0 ">
-                <div v-for="(post, index) in posts" :key="index" class="flex flex-col items-center lg:block lg:mx-0 mx-auto">
-                    <img :src="`http://localhost:3000/backend/images/${post.image}`" alt="Post Image" class="lg:mr-6 lg:mx-0 w-[537px] h-[358px] transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer mx-auto">
+    
+            <div v-if="posts.length" class="grid grid-cols-1 gap-10 lg:grid-cols-2 lg:gap-10 justify-center mx-auto lg:mx-0">
+                <div v-for="(post, index) in posts":key="index" class="flex flex-col items-center lg:block lg:mx-0 mx-auto">
+                    <img :src="`http://localhost:3000/backend/images/${post.image}`" alt="Post Image" class="lg:mr-6 lg:mx-0 w-[537px] h-[358px] transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer mx-auto"/>
                     <div class="flex mt-5">
                         <span class="text-txtGr">BY</span>&nbsp;&nbsp;{{ post.author }}&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;{{ post.category }}
                     </div>
-                    <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px] text-center lg:text-start">
-                        {{ post.title }}
-                    </div>
-                    <div class="text-txtGr text-base mt-[-13px] text-center lg:text-start">
-                        {{ post.description }}
-                    </div>
-                    <div class="text-txtGr text-base mt-[-13px] text-center lg:text-start">
-                        {{ post.excerpt }}
-                    </div>
+                    <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px] text-center lg:text-start">{{ post.title }}</div>
+                    <div class="text-txtGr text-base mt-[-13px] text-center lg:text-start">{{ post.description }}</div>
+                    <div class="text-txtGr text-base mt-[-13px] text-center lg:text-start">{{ post.excerpt }}</div>
                 </div>
             </div>
 
             <div v-else class="text-center mt-10">
                 <p>Loading posts...</p>
             </div>
-
+    
             <div class="mt-[182px] flex justify-center">
-                <button @click="loadMorePosts" class="mb-[155px] w-[204px] h-[70px] rounded-[40px] text-[#FF5480] border-2 border-[#FF5480] font-semibold hover:bg-opacity-90 text-2xl hover:bg-[#FF5480] hover:text-white transform transition-transform duration-300 hover:scale-105">
+                <button 
+                    @click="loadMorePosts" 
+                    class="mb-[155px] w-[204px] h-[70px] rounded-[40px] text-[#FF5480] border-2 border-[#FF5480] font-semibold hover:bg-opacity-90 text-2xl hover:bg-[#FF5480] hover:text-white transform transition-transform duration-300 hover:scale-105">
                     Load More
                 </button>
             </div>
@@ -54,38 +42,36 @@
         data() {
             return {
                 posts: [],
-                currentPage: 1,
-                type: "",
                 allPosts: [],
+                currentPage: 1,
+                pageSize: 4,
             };
         },
         created() {
             this.fetchPosts();
         },
         methods: {
-        async fetchPosts() {
-            try {
-            const response = await axios.get(`http://localhost:3000/posts?page=${this.currentPage}`);
-            this.posts = [...this.posts, ...response.data];
-            this.allPosts = [...this.allPosts, ...response.data];
-            } catch (error) {
-            console.error("Error fetching posts:", error);
-            }
+            async fetchPosts() {
+                try {
+                const response = await axios.get(`http://localhost:3000/posts`);
+                this.allPosts = response.data;
+                this.loadPosts();
+                } catch (error) {
+                console.error("Error fetching posts:", error);
+                }
+            },
+            loadPosts() {
+                const start = (this.currentPage - 1) * this.pageSize;
+                const end = this.currentPage * this.pageSize;
+                const newPosts = this.allPosts.slice(start, end);
+        
+                this.posts = [...this.posts, ...newPosts];
+            },
+            loadMorePosts() {
+                this.currentPage++;
+                this.loadPosts();
+            },
         },
-        loadMorePosts() {
-            this.currentPage++;
-            this.fetchPosts();
-        },
-        searchPosts() {
-            if (this.type.trim() === "") {
-                this.posts = [...this.allPosts];
-            } else {
-                this.posts = this.allPosts.filter(post => 
-                post.title.toLowerCase().includes(this.type.toLowerCase())
-                );
-            }
-        },
-    }
-};
+    };
 </script>
   
--- blog/src/pages/NewPage.vue
+++ blog/src/pages/NewPage.vue
@@ -1,13 +1,77 @@
 <template>
-    <div class="bg-custom-bg h-screen">
-        <div class="max-w-custom mx-auto">
-            <h1>Welcome to New Page</h1>
-        </div>
-    </div>
-  </template>
+  <div class="bg-custom-bg h-auto mx-auto p-8">
+      <div class="max-w-custom mx-auto">
+          <div class="text-center">
+              <div class="mt-10 mb-[16.8px] text-txtGr text-xs text-center lg:text-start">
+                  NEW POST
+              </div>
+          </div>
   
-  <script>
+          <div v-if="posts.length" class="grid grid-cols-1 gap-10 lg:grid-cols-2 lg:gap-10 justify-center mx-auto lg:mx-0">
+              <div v-for="(post, index) in posts":key="index" class="flex flex-col items-center lg:block lg:mx-0 mx-auto">
+                  <img :src="`http://localhost:3000/backend/images/${post.image}`" alt="Post Image" class="lg:mr-6 lg:mx-0 w-[537px] h-[358px] transform transition-transform duration-300 hover:scale-105 rounded-lg cursor-pointer mx-auto"/>
+                  <div class="flex mt-5">
+                      <span class="text-txtGr">BY</span>&nbsp;&nbsp;{{ post.author }}&nbsp;&nbsp;<span class="text-txtGr">IN</span>&nbsp;&nbsp;{{ post.category }}
+                  </div>
+                  <div class="text-[28px] font-semibold mt-5 h-[133px] mr-[37px] max-w-[536px] text-center lg:text-start">{{ post.title }}</div>
+                  <div class="text-txtGr text-base mt-[-13px] text-center lg:text-start">{{ post.description }}</div>
+                  <div class="text-txtGr text-base mt-[-13px] text-center lg:text-start">{{ post.excerpt }}</div>
+              </div>
+          </div>
+
+          <div v-else class="text-center mt-10">
+              <p>Chưa có bài đăng mới nào!</p>
+          </div>
+  
+          <div class="mt-[182px] flex justify-center">
+              <button 
+                  @click="loadMorePosts" 
+                  class="mb-[155px] w-[204px] h-[70px] rounded-[40px] text-[#FF5480] border-2 border-[#FF5480] font-semibold hover:bg-opacity-90 text-2xl hover:bg-[#FF5480] hover:text-white transform transition-transform duration-300 hover:scale-105">
+                  Load More
+              </button>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+  import axios from "axios";
+
   export default {
-    name: "NewPage",
+      name: "NewPage",
+      data() {
+          return {
+              posts: [],
+              allPosts: [],
+              currentPage: 1,
+              pageSize: 4,
+          };
+      },
+      created() {
+          this.fetchPosts();
+      },
+      methods: {
+          async fetchPosts() {
+            try {
+              const response = await axios.get('http://localhost:3000/posts');
+              const filteredPosts = response.data.filter(post => post.id > 14);
+              this.posts = filteredPosts;
+              this.allPosts = filteredPosts;
+            } catch (error) {
+              console.error("Error fetching posts:", error);
+            }
+          },
+          loadPosts() {
+              const start = (this.currentPage - 1) * this.pageSize;
+              const end = this.currentPage * this.pageSize;
+              const newPosts = this.allPosts.slice(start, end);
+      
+              this.posts = [...this.posts, ...newPosts];
+          },
+          loadMorePosts() {
+              this.currentPage++;
+              this.loadPosts();
+          },
+      },
   };
-  </script>
+</script>
